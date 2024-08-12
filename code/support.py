efa37f16fd01b9ffca8821099661fbbb40fa30a4
norapatches <info@norapatches.com>
2024-08-12 19:11:26 +0200

    Updated support docstrings and added return types

--- code/support.py
+++ code/support.py
@@ -1,47 +1,15 @@
-'''from settings import *
-from os import walk
-from os.path import join
-
-def import_image(*path, alpha= True, format= 'png'):
-    full_path = join(*path) + f'.{format}'
-    return pygame.image.load(full_path).convert_alpha() if alpha else pygame.image.load(full_path).convert()
-
-def import_folder(*path):
-    frames = []
-    for folder_path, subfolders, image_names in walk(join(*path)):
-        for image_name in sorted(image_names, key= lambda name: int(name.split('.')[0])):
-            full_path = join(folder_path, image_name)
-            frames.append(pygame.image.load(full_path).convert_alpha())
-    return frames
-
-def import_folder_dict(*path):
-    frame_dict = {}
-    for folder_path, _, image_names in walk(join(*path)):
-        for image_name in image_names:
-            full_path = join(folder_path, image_name)
-            surface = pygame.image.load(full_path).convert_alpha()
-            frame_dict[image_name.split('.')[0]] = surface
-    return frame_dict
-
-def import_sub_folders(*path):
-    frame_dict = {}
-    for _, sub_folders, __ in walk(join(*path)):
-        if sub_folders:
-            for sub_folder in sub_folders:
-                frame_dict[sub_folder] = import_folder(*path, sub_folder)
-    return frame_dict'''
-
-
 from settings import *
 from os import walk
 from os.path import join
 import pygame
 
-def import_image(*path, alpha=True, format='png'):
+def import_image(*path, alpha=True, format='png') -> pygame.Surface:
+    '''Import a single image from a file path'''
     full_path = join(*path) + f'.{format}'
     return pygame.image.load(full_path).convert_alpha() if alpha else pygame.image.load(full_path).convert()
 
-def import_folder(*path):
+def import_folder(*path) -> list:
+    '''Import images from a folder and store them in a list'''
     frames = []
     for folder_path, subfolders, image_names in walk(join(*path)):
         for image_name in sorted(image_names):
@@ -54,7 +22,8 @@ def import_folder(*path):
                 continue
     return frames
 
-def import_folder_dict(*path):
+def import_folder_dict(*path) -> dict:
+    '''Import images from a folder and store them in a dictionary'''
     frame_dict = {}
     for folder_path, _, image_names in walk(join(*path)):
         for image_name in image_names:
@@ -68,7 +37,8 @@ def import_folder_dict(*path):
                 continue
     return frame_dict
 
-def import_sub_folders(*path):
+def import_sub_folders(*path) -> dict:
+    '''Import assets from a folder and store them in a dictionary where the keys are the subfolder names'''
     frame_dict = {}
     for _, sub_folders, __ in walk(join(*path)):
         if sub_folders:
