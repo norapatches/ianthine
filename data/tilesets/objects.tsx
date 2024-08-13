b51b51c9942674748a09ef421534d5dd76c86c10
norapatches <info@norapatches.com>
2024-08-13 12:02:06 +0200

    made a creature asset in aseprite

---
+++ data/tilesets/objects.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="objects" tilewidth="16" tileheight="16" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../assets/graphic/player/idle/0.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../../assets/graphic/enemy/snail/0.png" width="16" height="8"/>
+ </tile>
+</tileset>
