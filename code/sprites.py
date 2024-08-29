981358d554fb62a112fb64e7fa187c0668cd5fa5
norapatches <info@norapatches.com>
2024-08-29 10:40:13 +0200

    updates to MovingSprite class

--- code/sprites.py
+++ code/sprites.py
@@ -45,9 +45,32 @@ class MovingSprite(AnimatedSprite):
         self.flip = flip
         self.reverse = {'x': False, 'y': False}
     
-    def check_border(self) -> None: pass
+    def check_border(self) -> None:
+        if self.move_dir == 'x':
+            if self.rect.right >= self.end_pos[0] and self.direction.x == 1:
+                self.direction.x = -1
+                self.rect.right = self.end_pos[0]
+            if self.rect.left <= self.start_pos[0] and self.direction.x == -1:
+                self.direction.x = 1
+                self.rect.left = self.start_pos[0]
+            self.reverse['x'] = True if self.direction.x < 0 else False
+        else:
+            if self.rect.bottom >= self.end_pos[1] and self.direction.y == 1:
+                self.direction.y = -1
+                self.rect.bottom = self.end_pos[1]
+            if self.rect.top <= self.start_pos[1] and self.direction.y == -1:
+                self.direction.y = 1
+                self.rect.top = self.start_pos[1]
+            self.reverse['y'] = True if self.direction.y > 0 else False
     
-    def update(self, dt) -> None: pass
+    def update(self, dt) -> None:
+        self.old_rect = self.rect.copy()
+        self.rect.topleft += self.direction * self.speed * dt
+        self.check_border()
+        
+        self.animate(dt)
+        if self.flip:
+            self.image = pygame.transform.flip(self.image, self.reverse['x'], self.reverse['y'])
 
 
 class Floor(Sprite):
