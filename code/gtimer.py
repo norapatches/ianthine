a726e609707762da6c9c72a50c2cf78c1297fdbb
norapatches <info@norapatches.com>
2024-09-03 15:15:18 +0200

    added docstrings to methods

--- code/gtimer.py
+++ code/gtimer.py
@@ -2,6 +2,7 @@ from pygame.time import get_ticks
 
 class Timer:
     def __init__(self, duration, func= None, repeat = False) -> None:
+        '''An instance of a timer that can also be used to execute a function'''
         self.duration = duration
         self.func = func
         self.start_time = 0
@@ -9,16 +10,19 @@ class Timer:
         self.repeat = repeat
     
     def start(self) -> None:
+        '''Start the timer'''
         self.active = True
         self.start_time = get_ticks()
     
     def stop(self) -> None:
+        '''Stop the timer. If repeat is enabled, the timer starts again'''
         self.active = False
         self.start_time = 0
         if self.repeat:
             self.start()
     
     def update(self) -> None:
+        '''The update method'''
         current_time = get_ticks()
         if current_time - self.start_time >= self.duration:
             if self.func and self.start_time != 0:
