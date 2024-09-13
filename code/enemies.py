f06d8becd7046c7cc42d65119e0aff33b7cca01d
norapatches <info@norapatches.com>
2024-09-13 16:57:23 +0200

    added Enemy class to inherit from, adjusted enemy type classes to inherit

--- code/enemies.py
+++ code/enemies.py
@@ -2,35 +2,45 @@ from settings import *
 from gtimer import Timer
 from random import choice
 
-'''Enemy types:
-    Chaser      img=frames[state][idx]     is idle until player near, moves towards player if on level, turns around on edges & walls
-    Crawler     img=frames[idx]            walks on surfaces, changes direction in corners
-    Floater     img=frames[state][idx]     moves around on level towards player if near, ignores collision and physics
-    Shooter     img=frames[state][idx]     stationary, shoots projectiles at player if near
-    *Skipper    img=frames[idx]            jumps around on level endlessly
-    *Walker     img=frames[idx]            walks left-right, turns around on edges & walls
-
-Using these enemy archetypes we can use different assets and have more enemies in total.
-'''
-
-# ENEMY TYPES
-class Chaser(pygame.sprite.Sprite):
-    '''Stays asleep until player is nearby, goes idle if player is near on x axis, walks if player is near on both x, y axies'''
-    def __init__(self, position, frames, groups, collision_sprites, player) -> None:
-        super().__init__(groups)
+# BLUEPRINT CLASS TO INHERIT FROM
+class Enemy(pygame.sprite.Sprite):
+    '''Enemy types & states
+        Chaser      [asleep, idle, charge, walk, *death]
+        Crawler     [idle, walk, *death]
+        Floater     [asleep, idle, walk, *death]
+        Shooter     [asleep, idle, notice, shoot, *death]
+        Skipper     [idle, jump, *death]
+        Walker      [idle, walk, *death]
+    '''
+    def __init__(self, position, frames, groups) -> None:
         self.enemy = True
+        super().__init__(groups)
         
         self.frames, self.frame_index = frames, 0
-        self.state, self.facing_right = 'asleep', True
+        self.state, self.facing_right = 'idle', True
         self.image = self.frames[self.state][self.frame_index]
         
+        self.mask = pygame.mask.from_surface(self.image)
+        
         self.rect = self.image.get_frect(topleft= position)
-        self.hitbox_rect = self.rect.inflate(-6, 0)
-        self.old_rect = self.hitbox_rect.copy()
+        self.old_rect = self.rect.copy()
         self.z = Z_LAYERS['main']
         
         self.direction = vector()
-        self.speed = 36
+        self.speed = 16
+    
+    def take_hit(self) -> None:
+        pass
+
+
+# ENEMY TYPES
+class Chaser(Enemy):
+    '''Chaser enemy is asleep until player near, moves towards player if on level, stops near edges & walls'''
+    def __init__(self, position, frames, groups, collision_sprites, player) -> None:
+        super().__init__(position, frames, groups)
+        self.hitbox_rect = self.rect.inflate(-4, 0)
+        self.old_rect = self.hitbox_rect.copy()
+        self.speed = 32
         
         self.collision_rects = [sprite.rect for sprite in collision_sprites]
         self.player = player
@@ -40,17 +50,21 @@ class Chaser(pygame.sprite.Sprite):
         self.timers = {'hit': Timer(400), 'edge': Timer(600)}
     
     def take_hit(self) -> None:
+        '''Move backwards when hit by player'''
         if not self.timers['hit'].active:
             self.direction.x *= -1
             self.hitbox_rect.move_ip(-16, 0) if self.direction.x < 0 else self.hitbox_rect.move_ip(16, 0)
             self.timers['hit'].start()
     
     def check_player_near(self) -> None:
+        '''Check if player is nearby'''
         player_pos, chaser_pos = vector(self.player.hitbox_rect.center), vector(self.hitbox_rect.center)
+        
         self.player_near['x'] = chaser_pos.distance_to(player_pos) <= 64
         self.player_near['y'] = abs(chaser_pos.y - player_pos.y) <= 16
     
     def check_contact(self) -> None:
+        '''Check contact with walls and edges'''
         floor_rect_right = pygame.FRect((self.hitbox_rect.bottomright), (2, 2))
         floor_rect_left = pygame.FRect((self.hitbox_rect.bottomleft + vector(-2, 0)), (2, 2))
         wall_rect = pygame.FRect((self.hitbox_rect.topleft + vector(-2, 0)), (self.hitbox_rect.width + 4, 2))
@@ -61,7 +75,21 @@ class Chaser(pygame.sprite.Sprite):
             self.timers['edge'].start()
             self.hitbox_rect.move_ip(-1, 0) if self.direction.x > 0 else self.hitbox_rect.move_ip(1, 0)
     
+    def get_state(self) -> None:
+        '''Get states for animation'''
+        self.state = 'asleep'
+        if self.player_near['x'] and not self.player_near['y'] and not self.player.crouch:
+            self.state = 'idle'
+        if self.player_near['x'] and self.player_near['y'] and not self.player.crouch:
+            self.state = 'walk'
+    
+    def update_timers(self) -> None:
+        '''Update all timers'''
+        for timer in self.timers.values():
+            timer.update()
+    
     def move(self, dt) -> None:
+        '''The movement method'''
         if not self.timers['edge'].active:
             if self.player_near['x'] and self.player_near['y'] and not self.player.crouch:
                 self.direction.x = -1 if self.player.hitbox_rect.centerx <= self.hitbox_rect.centerx else 1
@@ -70,51 +98,42 @@ class Chaser(pygame.sprite.Sprite):
                 self.check_contact()
             self.rect.center = self.hitbox_rect.center
     
-    def get_state(self) -> None:
-        self.state = 'asleep'
-        if self.player_near['x'] and not self.player_near['y'] and not self.player.crouch:
-            self.state = 'idle'
-        if self.player_near['x'] and self.player_near['y'] and not self.player.crouch:
-            self.state = 'walk'
-    
     def animate(self, dt) -> None:
+        '''The animation method'''
         self.frame_index += ANIMATION_SPEED * dt
         self.image = self.frames[self.state][int(self.frame_index % len(self.frames[self.state]))]
         self.image = pygame.transform.flip(self.image, True, False) if self.direction.x < 0 else self.image
-    
-    def update_timers(self) -> None:
-        for timer in self.timers.values():
-            timer.update()
+        
+        self.mask = pygame.mask.from_surface(self.image)
     
     def update(self, dt) -> None:
+        '''The update method'''
         self.old_rect = self.hitbox_rect.copy()
-        
         self.update_timers()
-        self.check_player_near()
         
+        self.check_player_near()
         self.move(dt)
         
         self.get_state()
         self.animate(dt)
 
-class Crawler(pygame.sprite.Sprite):
-    '''The Crawler can move on any surface endlessly'''
+class Crawler(Enemy):
+    '''Crawler enemy moves around on surfaces endlessly'''
     def __init__(self, position, frames, groups, collision_sprites) -> None:
-        super().__init__(groups)
-        self.frames, self.frame_index = frames, 0
-        self.image = self.frames[self.frame_index]
-        self.rect = self.image.get_frect(topleft= position)
-        self.old_rect = self.rect.copy()
-        self.z = Z_LAYERS['main']
-        
-        self.direction = vector(1, 0)
+        super().__init__(position, frames, groups)
+        self.hitbox_rect = self.rect.inflate(0, 0)
+        self.old_rect = self.hitbox_rect.copy()
         self.speed = 8
+        self.direction = vector(1, 0)
+        self.state = 'walk'
+        
         self.collision_rects = [sprite.rect for sprite in collision_sprites]
         
         self.on_surface = {'bottom': False, 'top': False, 'left': False, 'right': False}
         self.rotate = {'left': False, 'right': False}
-        
+    
     def check_contact(self) -> None:
+        '''Check contact with walss and edges'''
         top_rect = pygame.FRect((self.rect.topleft + vector(0, -1)), (self.rect.width, 1))
         bottom_rect = pygame.FRect((self.rect.bottomleft), (self.rect.width, 1))
         left_rect = pygame.FRect((self.rect.topleft + vector(-1, 0)), (1, self.rect.height))
@@ -126,10 +145,11 @@ class Crawler(pygame.sprite.Sprite):
         self.on_surface['right'] = True if right_rect.collidelist(self.collision_rects) >= 0 else False
     
     def move(self, dt) -> None:
+        '''The movement method'''
         self.rect.topleft += self.direction * self.speed * dt
     
     def change_move_dir(self) -> None:
-        
+        '''Detect collision with terrain and change move direction'''
         if self.on_surface['right']:
             self.rotate['left'] = True
         elif self.on_surface['left']:
@@ -154,14 +174,18 @@ class Crawler(pygame.sprite.Sprite):
             self.direction.x = 1
     
     def animate(self, dt) -> None:
+        '''The animation method'''
         self.frame_index += ANIMATION_SPEED * dt
-        self.image = self.frames[int(self.frame_index % len(self.frames))]
+        self.image = self.frames[self.state][int(self.frame_index % len(self.frames[self.state]))]
         
         self.image = pygame.transform.rotate(self.image, 90) if self.rotate['left'] else self.image
         self.image = pygame.transform.rotate(self.image, -90) if self.rotate['right'] else self.image
         self.image = pygame.transform.flip(self.image, True, True) if self.on_surface['top'] else self.image
+        
+        self.mask = pygame.mask.from_surface(self.image)
     
     def update(self, dt) -> None:
+        '''The update method'''
         self.old_rect = self.rect.copy()
         self.check_contact()
         
@@ -170,31 +194,21 @@ class Crawler(pygame.sprite.Sprite):
         
         self.animate(dt)
 
-class Floater(pygame.sprite.Sprite):
+class Floater(Enemy):
+    '''Floater enemy is asleep until player is near, moves towards player ignoring collisions if player is near and facing away'''
     def __init__(self, position, frames, groups, player) -> None:
-        super().__init__(groups)
-        self.enemy = True
-        
-        self.frames, self.frame_index = frames, 0
-        self.state, self.facing_right = 'asleep', True
-        self.image = self.frames[self.state][self.frame_index]
-        
-        self.rect = self.image.get_frect(topleft= position)
+        super().__init__(position, frames, groups)
         self.hitbox_rect = self.rect.inflate(-6, -2)
         self.old_rect = self.hitbox_rect.copy()
-        self.z = Z_LAYERS['main']
         
-        self.direction = vector()
         self.facing_right = True
         self.speed = 36
         
         self.player = player
         self.player_near = {'x': False, 'y': False, 'facing_away': False}
     
-    def take_hit(self) -> None:
-        pass
-    
     def check_player_near(self) -> None:
+        '''Check if player is nearby'''
         player_pos, floater_pos = vector(self.player.hitbox_rect.center), vector(self.hitbox_rect.center)
         self.player_near['x'] = floater_pos.distance_to(player_pos) <= 80
         self.player_near['y'] = abs(floater_pos.y - player_pos.y) <= 80
@@ -206,6 +220,7 @@ class Floater(pygame.sprite.Sprite):
             (self.hitbox_rect.centerx > self.player.hitbox_rect.centerx and not self.facing_right and not self.player.facing_right)) else False
     
     def get_state(self) -> None:
+        '''Get states for animation'''
         self.state = 'asleep'
         if all((self.player_near['x'], self.player_near['y'])) and not self.player_near['facing_away']:
             self.state = 'idle'
@@ -213,6 +228,7 @@ class Floater(pygame.sprite.Sprite):
             self.state = 'move'
 
     def move(self, dt) -> None:
+        '''The movement method'''
         if self.state == 'move':
             self.direction.x = 1 if self.hitbox_rect.centerx <= self.player.hitbox_rect.centerx else -1
             self.direction.y = 1 if self.hitbox_rect.centery <= self.player.hitbox_rect.centery else -1
@@ -221,11 +237,13 @@ class Floater(pygame.sprite.Sprite):
             self.rect.center = self.hitbox_rect.center
     
     def animate(self, dt) -> None:
+        '''The animation method'''
         self.frame_index += ANIMATION_SPEED * dt
         self.image = self.frames[self.state][int(self.frame_index % len(self.frames[self.state]))]
         self.image = pygame.transform.flip(self.image, True, False) if not self.facing_right else self.image
     
     def update(self, dt) -> None:
+        '''The update method'''
         self.old_rect = self.hitbox_rect.copy()
         
         self.check_player_near()
@@ -234,21 +252,12 @@ class Floater(pygame.sprite.Sprite):
         self.move(dt)
         self.animate(dt)
 
-class Shooter(pygame.sprite.Sprite):
+class Shooter(Enemy):
+    '''Shooter enemy is stationary and asleep until player is near, turns towards player and shoots projectile is player is near'''
     def __init__(self, position, frames, groups, player, create_projectile) -> None:
-        self.enemy = True
-        super().__init__(groups)
-        
-        self.frames, self.frame_index = frames, 0
-        self.state, self.facing_right = 'asleep', True
-        self.image = self.frames[self.state][self.frame_index]
-        
+        super().__init__(position, frames, groups)
+        self.state = 'asleep'
         self.facing_right = True
-        
-        self.rect = self.image.get_frect(topleft= position)
-        self.hitbox_rect = pygame.FRect()
-        self.old_rect = self.rect.copy()
-        self.z = Z_LAYERS['main']
         
         self.player = player
         
@@ -257,6 +266,7 @@ class Shooter(pygame.sprite.Sprite):
         self.create_projectile = create_projectile
     
     def check_player_near(self) -> None:
+        '''Check is playeris nearby'''
         player_pos, shooter_pos = vector(self.player.hitbox_rect.center), vector(self.rect.center)
         player_near = shooter_pos.distance_to(player_pos) < 64
         player_level = abs(shooter_pos.y - player_pos.y) < 32
@@ -268,7 +278,7 @@ class Shooter(pygame.sprite.Sprite):
             self.facing_right = False if shooter_pos.x > player_pos.x else True
     
     def animate(self, dt) -> None:
-        
+        '''The animation method'''
         self.frame_index += (ANIMATION_SPEED + 2) * dt
         if self.frame_index < len(self.frames[self.state]):
             self.image = self.frames[self.state][int(self.frame_index)]
@@ -285,47 +295,42 @@ class Shooter(pygame.sprite.Sprite):
                 self.has_fired = False
     
     def update(self, dt) -> None:
+        '''The update method'''
         self.old_rect = self.rect.copy()
         self.timers['shoot'].update()
+        
         self.check_player_near()
-        print(self.state)
+        
         self.animate(dt)
 
-class Skipper(pygame.sprite.Sprite): pass
-
-class Walker(pygame.sprite.Sprite):
+class Skipper(Enemy):
+    '''Skipper enemy is jumping around the level endlessly, has full collision with all surfaces'''
     def __init__(self, position, frames, groups, collision_sprites) -> None:
-        super().__init__(groups)
-        self.enemy = True
+        super().__init__(position, frames, groups)
+    
+    def update(self, dt) -> None:
+        pass
+
+
+class Walker(Enemy):
+    '''Walker enemy walks around left and right and turns around on edges and walls'''
+    def __init__(self, position, frames, groups, collision_sprites) -> None:
+        super().__init__(position, frames, groups)
         
-        self.frames, self.frame_index = frames, 0
-        self.image = self.frames[self.frame_index]
-        self.rect = self.image.get_frect(topleft= position)
         self.hitbox_rect = self.rect.inflate(-6, 0)
-        self.z = Z_LAYERS['main']
-        
-        self.direction = choice((-1, 1))
+        self.old_rect = self.hitbox_rect.copy()
+        self.speed = 32
+        self.direction = vector(choice((-1, 1)), 0)
+        self.state = 'walk'
         self.collision_rects = [sprite.rect for sprite in collision_sprites]
         
-        self.speed = 32
         self.hit_timer = Timer(250)
     
     def take_hit(self) -> None:
-        '''Take hit form player attack'''
+        '''Take hit from player attack'''
         if not self.hit_timer.active:
-            self.direction *= -1
+            self.direction.x *= -1
             self.hit_timer.start()
-    
-    def move(self, dt) -> None:
-        '''The mobve method'''
-        self.hitbox_rect.x += self.direction * self.speed * dt
-        self.rect.center = self.hitbox_rect.center
-    
-    def animate(self, dt) -> None:
-        '''Animate movement'''
-        self.frame_index += ANIMATION_SPEED * dt
-        self.image = self.frames[int(self.frame_index % len(self.frames))]
-        self.image = pygame.transform.flip(self.image, True, False) if self.direction < 0 else self.image
     
     def check_contact(self) -> None:
         '''Turn around on edges and walls'''
@@ -333,17 +338,28 @@ class Walker(pygame.sprite.Sprite):
         floor_rect_left = pygame.FRect(self.hitbox_rect.bottomleft, (-1, 1))
         wall_rect = pygame.FRect(self.hitbox_rect.topleft + vector(-1, 0), (self.hitbox_rect.width + 2, 1))
         
-        if floor_rect_right.collidelist(self.collision_rects) < 0 and self.direction > 0 or\
-            floor_rect_left.collidelist(self.collision_rects) < 0 and self.direction < 0 or\
+        if floor_rect_right.collidelist(self.collision_rects) < 0 and self.direction.x > 0 or\
+            floor_rect_left.collidelist(self.collision_rects) < 0 and self.direction.x < 0 or\
             wall_rect.collidelist(self.collision_rects) != -1:
-            self.direction *= -1
+            self.direction.x *= -1
+    
+    def move(self, dt) -> None:
+        '''The mobve method'''
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
+        self.rect.center = self.hitbox_rect.center
+    
+    def animate(self, dt) -> None:
+        '''Animate movement'''
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[self.state][int(self.frame_index % len(self.frames))]
+        self.image = pygame.transform.flip(self.image, True, False) if self.direction.x < 0 else self.image
     
     def update(self, dt) -> None:
         self.hit_timer.update()
         
         self.check_contact()
         self.move(dt)
-        self.animate(dt)      
+        self.animate(dt)  
 
 
 # ENEMY PROJECTILES
