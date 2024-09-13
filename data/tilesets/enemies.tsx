eeb2a62765ef02e8cdfe305cbc11d3d5dd7f03cf
norapatches <info@norapatches.com>
2024-09-13 19:31:44 +0200

    updated assets and enemies

--- data/tilesets/enemies.tsx
+++ data/tilesets/enemies.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="enemies" tilewidth="16" tileheight="16" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="enemies" tilewidth="16" tileheight="16" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../../assets/graphic/objects/plant.png" width="16" height="16"/>
@@ -12,5 +12,11 @@
  </tile>
  <tile id="3">
   <image source="../../assets/graphic/objects/horn.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../../assets/graphic/objects/skeleton.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../../assets/graphic/objects/slimeball.png" width="8" height="8"/>
  </tile>
 </tileset>
