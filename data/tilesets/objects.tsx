41d1f2e7f9e5a4e5cc451174bca9086d3b58d4a0
norapatches <info@norapatches.com>
2024-09-18 12:46:59 +0200

    added door assets

--- data/tilesets/objects.tsx
+++ data/tilesets/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="objects" tilewidth="64" tileheight="64" tilecount="15" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="objects" tilewidth="62" tileheight="62" tilecount="9" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../../assets/graphic/objects/player.png" width="16" height="16"/>
@@ -7,23 +7,8 @@
  <tile id="1">
   <image source="../../assets/graphic/objects/snail.png" width="16" height="8"/>
  </tile>
- <tile id="2">
-  <image source="../../assets/graphic/objects/ghost.png" width="16" height="16"/>
- </tile>
- <tile id="3">
-  <image source="../../assets/graphic/objects/door.png" width="16" height="16"/>
- </tile>
- <tile id="4">
-  <image source="../../assets/graphic/objects/human.png" width="16" height="16"/>
- </tile>
  <tile id="5">
   <image source="../../assets/graphic/objects/creature.png" width="16" height="16"/>
- </tile>
- <tile id="6">
-  <image source="../../assets/graphic/objects/shadowman.png" width="16" height="16"/>
- </tile>
- <tile id="7">
-  <image source="../../assets/graphic/objects/soldier.png" width="16" height="16"/>
  </tile>
  <tile id="8">
   <image source="../../assets/graphic/objects/key.png" width="16" height="16"/>
@@ -34,16 +19,13 @@
  <tile id="10">
   <image source="../../assets/graphic/objects/checkpoint.png" width="16" height="16"/>
  </tile>
- <tile id="11">
-  <image source="../../assets/graphic/objects/crawler.png" width="8" height="8"/>
- </tile>
  <tile id="12">
   <image source="../../assets/graphic/objects/coin.png" width="8" height="8"/>
  </tile>
- <tile id="13">
-  <image source="../../assets/graphic/objects/horn.png" width="16" height="16"/>
- </tile>
  <tile id="14">
-  <image source="../../assets/graphic/objects/golem.png" width="64" height="64"/>
+  <image source="../../assets/graphic/objects/golem.png" width="62" height="62"/>
+ </tile>
+ <tile id="15">
+  <image source="../../assets/graphic/objects/door.png" width="16" height="16"/>
  </tile>
 </tileset>
