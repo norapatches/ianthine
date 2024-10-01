b6f71aef7144f64751b67015357fc65ff7ad8da7
norapatches <info@norapatches.com>
2024-10-01 19:26:43 +0200

    added arrow class

--- code/player.py
+++ code/player.py
@@ -28,11 +28,12 @@ class Player(pygame.sprite.Sprite):
         
         # rects
         self.rect = self.image.get_frect(topleft = position)
-        self.hitbox_rect = self.rect.inflate(-8, 0)
+        self.hitbox_rect = pygame.FRect((self.rect.topleft + vector(13, 16)), (11, 16))
         self.old_rect = self.hitbox_rect.copy()
                
         # movement
         self.direction = vector()
+        self.speed = 64
         
         self.fallspeed_max = 256
         self.gravity = 1024
@@ -57,7 +58,7 @@ class Player(pygame.sprite.Sprite):
             'platform_skip': Timer(100),
             'walljump': Timer(150),
             'wallslide_block': Timer(400),
-            'attack_lock': Timer(600)
+            'attack_lock': Timer(450)
         }
     
     def input(self) -> None:
@@ -70,11 +71,11 @@ class Player(pygame.sprite.Sprite):
             # we ignore input for a short time while jumping off the wall
             if not self.timers['walljump'].active:
                 # movement
-                if pressed[self.controls.right]:
+                if pressed[self.controls.right] and not self.state in ('melee', 'ranged'):
                     input_vector.x += 1
                     self.facing_right = True
                 
-                if pressed[self.controls.left]:
+                if pressed[self.controls.left] and not self.state in ('melee', 'ranged'):
                     input_vector.x -= 1
                     self.facing_right = False
                 
@@ -95,11 +96,13 @@ class Player(pygame.sprite.Sprite):
                     self.interaction['do'] = False
                 
                 # melee
-                if jpressed[self.controls.melee]:
+                if jpressed[self.controls.melee] and not self.state in ('melee', 'air_melee'):
                     self.attack('melee')
+                    self.frame_index = 0
                 # ranged
-                if jpressed[self.controls.ranged]:
+                if jpressed[self.controls.ranged] and not self.state == 'ranged':
                     self.attack('ranged')
+                    self.frame_index = 0
                 
                 self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
             
@@ -131,8 +134,6 @@ class Player(pygame.sprite.Sprite):
                 self.timers['attack_lock'].start()
     
     def move(self, dt) -> None:
-        self.speed = 64
-        self.speed = self.speed if not self.crouch else self.speed / 2
         
         # horizontal movement
         self.hitbox_rect.x += self.direction.x * self.speed * dt
@@ -167,7 +168,7 @@ class Player(pygame.sprite.Sprite):
         self.collision('vertical')
         self.semi_collision()
         
-        self.rect.center = self.hitbox_rect.center
+        self.rect.topleft = self.hitbox_rect.topleft + vector(-13, -16)
         self.map_rect.x, self.map_rect.y = self.hitbox_rect.x / TILE_SIZE, self.hitbox_rect.y / TILE_SIZE
     
     def platform_move(self, dt) -> None:
@@ -238,16 +239,18 @@ class Player(pygame.sprite.Sprite):
             timer.update()
     
     def animate(self, dt) -> None:
-        if self.state in ['melee', 'air_melee', 'ranged', 'air_ranged']:
-            self.frame_index += ANIMATION_SPEED * 2.5 * dt
+        if self.state in ['melee', 'ranged']:
+            self.frame_index += (ANIMATION_SPEED * 2) * dt
+        elif self.state == 'idle':
+            self.frame_index += (ANIMATION_SPEED * 0.75) * dt
         else:
             self.frame_index += ANIMATION_SPEED * dt
         
-        if self.state in ['ranged', 'air_ranged'] and int(self.frame_index) == 1 and not self.has_fired:
+        if self.state == 'ranged' and int(self.frame_index) == 1 and not self.has_fired:
             self.create_projectile(self.hitbox_rect.center, 1 if self.facing_right else -1)
             self.has_fired = True
         
-        if self.state in ['melee', 'air_melee', 'ranged', 'air_ranged'] and self.frame_index >= len(self.frames[self.state]):
+        if self.state in ['melee', 'ranged'] and self.frame_index >= len(self.frames[self.state]):
             self.state = 'idle'
             self.has_fired = False
         
@@ -273,14 +276,14 @@ class Player(pygame.sprite.Sprite):
             if self.melee_atk:
                 self.state = 'air_melee'
             elif self.ranged_atk:
-                self.state = 'air_ranged'
+                self.state = 'ranged'
             else:
                 if any((self.on_surface['left'], self.on_surface['right'])):
                         self.state = 'wallslide'
                 else:
                     self.state = 'jump' if self.direction.y < 0 else 'fall'
         if self.crouch and self.direction.x != 0 and self.direction.y == 0:
-            self.state = 'crouch_walk'
+            self.state = 'walk'
     
     def show_hitbox(self) -> None:
         surf = pygame.Surface(self.hitbox_rect.size)
@@ -310,7 +313,6 @@ class Player(pygame.sprite.Sprite):
         self.platform_move(dt)
         self.check_contact()
         
-        
         # animation
         self.get_state()
         self.animate(dt)
@@ -320,13 +322,15 @@ class Player(pygame.sprite.Sprite):
         #self.show_collision_detect()
 
 
-class Projectile(pygame.sprite.Sprite):
-    def __init__(self, position, groups, direction, speed) -> None:
+class Arrow(pygame.sprite.Sprite):
+    def __init__(self, position, frames, groups, direction, speed) -> None:
         self.projectile = True
         super().__init__(groups)
-        self.image = pygame.Surface((2, 2))
-        self.image.fill('white')
-        self.rect = self.image.get_frect(center= position + vector(6 * direction, -3))
+        self.frames, self.frame_index = frames, 0
+        self.image = self.frames[self.frame_index]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_frect(center= position + vector(6 * direction, 0))
+        self.hitbox_rect = self.rect.inflate(0, 10)
         self.direction = direction
         self.speed = speed
         self.z = Z_LAYERS['main']
@@ -336,10 +340,19 @@ class Projectile(pygame.sprite.Sprite):
         }
         self.timers['lifetime'].start()
     
+    def animate(self, dt) -> None:
+        self.frame_index += (ANIMATION_SPEED * 1.5) * dt
+        self.image = self.frames[int(self.frame_index % len(self.frames))]
+        self.image = pygame.transform.flip(self.image, True, False) if self.direction < 0 else self.image
+        self.mask = pygame.mask.from_surface(self.image)
+    
     def update(self, dt) -> None:
         for timer in self.timers.values():
             timer.update()
-        self.rect.x += self.direction * self.speed * dt
+        
+        self.animate(dt)
+        self.hitbox_rect.x += self.direction * self.speed * dt
+        self.rect.center = self.hitbox_rect.center
         
         if not self.timers['lifetime'].active:
             self.kill()
