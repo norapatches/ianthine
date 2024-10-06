6df4ade387f1b0c866e22321072f23aeaa940ccc
norapatches <info@norapatches.com>
2024-10-06 11:45:47 +0200

    added game settings

--- code/gdata.py
+++ code/gdata.py
@@ -6,31 +6,50 @@ class GameData:
     def __init__(self, ui) -> None:
         self.ui = ui
         
-        self._coins = 0
-        self.key = False
+        # PLAYER
+        self._health: int = 5
+        self._coins: int = 0
+        self._gems: int = 0
+        self.key: bool = False
         
-        self._health = 5
-        self.ui.create_hearts(self._health)
+        # GAME PROGRESS
+        self.unlocked_level: int = 0
+        self.current_level: int = 0
         
-        self.unlocked_level = 0
-        self.current_level = 0
+        # GRAPHIC SETTINGS
+        self.graphic_filter = None
+        self.filter_invert: bool = False
         
-        self.paused = False
+        # AUDIO SETTINGS
+        self.bgm_volume: float = 1.0
+        self.sfx_volume: float = 1.0
+        
+        # CONTROL SETTINGS
+        self.control_scheme: int = 0
+        
+        # PAUSE/RESUME
+        self.paused: bool = False
     
     @property
     def health(self) -> int:
         return self._health
     
     @health.setter
-    def health(self, value) -> None:
+    def health(self, value: int) -> None:
         self._health = value
-        self.ui.create_hearts(value)
 
     @property
     def coins(self):
         return self._coins
     
     @coins.setter
-    def coins(self, value):
+    def coins(self, value: int):
         self._coins = value
     
+    @property
+    def gems(self) -> int:
+        return self._gems
+    
+    @gems.setter
+    def gems(self, value: int) -> None:
+        self._gems = value
